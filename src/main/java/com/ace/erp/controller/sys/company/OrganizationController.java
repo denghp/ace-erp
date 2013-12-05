ed3ebd5ed3b5dc899132dd685ed4b2d5f8c158d7
denghp <denghp@126.com>
2013-12-05 18:53:36 +0800

    update company page

--- src/main/java/com/ace/erp/controller/sys/company/OrganizationController.java
+++ src/main/java/com/ace/erp/controller/sys/company/OrganizationController.java
@@ -30,6 +30,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -110,6 +111,16 @@ public class OrganizationController extends BaseController<Organization, Integer
         responseJson.setTotal(totalPage);
         responseJson.setRecords(totalRecord);
         return responseJson;
+    }
+
+    @RequestMapping(value="/company/upload",method = RequestMethod.POST)
+    @ResponseBody
+    public Response upload(MultipartHttpServletRequest request, HttpServletResponse response) {
+
+        request.getFileNames();
+
+
+        return null;
     }
 
 }
